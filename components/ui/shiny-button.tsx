d56d87adc355e018fb209ca48db0d21b9b538ca0
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2025-07-19 03:32:01 +0500

    Update shiny-button.tsx

--- components/ui/shiny-button.tsx
+++ components/ui/shiny-button.tsx
@@ -29,20 +29,17 @@ const animationProps: MotionProps = {
 interface ShinyButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   className?: string;
-  motionProps?: MotionProps;
 }
 
 const ShinyButton = ({ 
   children, 
-  className, 
-  motionProps,
-  ...props 
+  className,
+  ...buttonProps 
 }: ShinyButtonProps) => {
   return (
     <motion.button
       {...animationProps}
-      {...motionProps}
-      {...props}
+      {...buttonProps}
       className={cn(
         "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
         className,
