f4ab2b067ade172eb53acf6f7d4c745d6330244c
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2025-10-30 23:35:41 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@
 // next.config.mjs
 const nextConfig = {
     output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/maq796113.github.io',
     trailingSlash: true
   };
 
